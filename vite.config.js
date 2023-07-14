07c7badc52a737fa47fdb7fe0caec8f20c03d5d0
Cris <crisjohnreytarpin@gmail.com>
2023-07-14 10:57:33 +0100

    updates

--- vite.config.js
+++ vite.config.js
@@ -21,7 +21,7 @@ export default defineConfig({
     build: {
         rollupOptions: {
             output: {
-                manualChunks: {}
+                manualChunks: undefined
             }
         }
     }
