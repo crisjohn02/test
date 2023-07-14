14c0046717c18ee507da5f11ae6266edc2b8d11f
Cris <crisjohnreytarpin@gmail.com>
2023-07-14 09:27:38 +0100

    updates

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    base: 'test/build',
     plugins: [
         laravel({
             input: 'resources/js/app.js',
